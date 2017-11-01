bb279249cdf0a216a08bd8babb6eaaef59bf09c2
Bond Davis <bonddavis92@gmail.com>
2017-11-01 10:30:35 -0500

    categories back-end completed

--- server/api/category.js
+++ server/api/category.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
-const {Category} = require('../db/models')
+const { Category } = require('../db/models')
 module.exports = router
 
 
 router.param('categoryId', (req, res, next, categoryId) => {
   Category.findById(categoryId)
-  .then(review => {
-    req.selectedCategory = category;
-    next();
-  })
-  .catch(next)
+    .then(category => {
+      req.selectedCategory = category;
+      next();
+    })
+    .catch(next)
 })
 
 router.get('/', (req, res, next) => {
@@ -20,13 +20,17 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/:categoryId', (req, res, next) => {
-  res.status(200).json(req.selectedCategory);
+  req.selectedCategory.reload()
+    .then((category) => {
+      res.json(category).status(200);
+    })
+    .catch(next)
 });
 
 
-router.post('/newCategory', (req, res, next) => {
+router.post('/', (req, res, next) => {
   const category = req.body;
-  Review.create(category)
+  Category.create(category)
     .then(category => {
       res.status(201).json(category);
     })
@@ -35,30 +39,17 @@ router.post('/newCategory', (req, res, next) => {
 
 router.put('/:categoryId', (req, res, next) => {
   req.selectedCategory.update(req.body)
-    .then(
-    res.status(201)
-    )
-    .done();
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch(next);
 })
 
 router.delete('/:categoryId', (req, res, next) => {
-  const categoryId = req.params.categoryId;
-
   req.selectedCategory.destroy()
-  .then(() => {
-    res.status(204).send('deleted category').end();
-  })
-  
-  .catch(next)
-  
-  
-  findById(categoryId)
-  .then(category => {
-    return category.destroy()
-  })
-  .then(() => {
-    return res.status(204);
-  })
-  .catch(next);
-  })
+    .then((category) => {
+      res.status(204).send(category);
+    })
+    .catch(next)
+})
 
--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'));
 router.use('/reviews', require('./review'));
-
+router.use('/categories', require('./category'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
