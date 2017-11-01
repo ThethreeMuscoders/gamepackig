a013f4cfd466b7b38d0c9248638529b07091dd44
Damian Michniak <damian.michniak29@gmail.com>
2017-10-31 20:14:39 -0500

    Completed test specs for product routes

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -1,33 +1,137 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const User = db.model('user')
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
 
-describe('Product routes', () => {
+const Product = db.model('product');
+
+describe('Product Routes', () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    return db.sync({ force: true });
+  });
 
   describe('/api/products/', () => {
-    const codysEmail = 'cody@puppybook.com'
+    const productObj = {
+      name: 'computer1',
+      image: 'randomimg',
+      description: 'a really god pc',
+      price: 1500000.51,
+      quantity: 5,
+      categoryId: 1,
+    };
 
-    beforeEach(() => {
-      return User.create({
-        email: codysEmail
+    let productId;
+
+    beforeEach((done) => {
+      Product.create(productObj)
+        .then((product) => {
+          productId = product.id;
+          done();
+        });
+    });
+
+    afterEach((done) => {
+      Product.destroy({
+        where: {
+          id: productId,
+        },
       })
-    })
+        .then(() => {
+          done();
+        });
+    });
 
-    it('GET /api/users', () => {
+    it('GET /api/products', () => {
       return request(app)
-        .get('/api/users')
+        .get('/api/products/')
         .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(codysEmail)
-        })
-    })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+        .then(({ body }) => {
+          expect(body).to.be.an('array')
+          expect(body[0].name).to.be.equal(productObj.name);
+          expect(body[0].image).to.be.equal(productObj.image);
+          expect(body[0].description).to.be.equal(productObj.description);
+          expect(body[0].price).to.be.equal(productObj.price);
+          expect(body[0].quantity).to.be.equal(productObj.quantity);
+          expect(body[0].categoryId).to.be.equal(productObj.categoryId);
+        });
+    });
+
+    it('GET /api/products/:productId', () => {
+      return request(app)
+        .get(`/api/products/${productId}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).to.be.an('object');
+          expect(body.name).to.be.equal(productObj.name);
+          expect(body.image).to.be.equal(productObj.image);
+          expect(body.description).to.be.equal(productObj.description);
+          expect(body.price).to.be.equal(productObj.price);
+          expect(body.quantity).to.be.equal(productObj.quantity);
+          expect(body.categoryId).to.be.equal(productObj.categoryId);
+        });
+    });
+
+    it('POST /api/products', () => {
+      const productToPost = {
+        name: 'graphic card',
+        image: 'image 8',
+        description: 'super awesome card',
+        price: 4.51,
+        quantity: 2,
+        categoryId: 2,
+      };
+
+      return request(app)
+        .post('/api/products/')
+        .send(productToPost)
+        .expect(201)
+        .then(({ body }) => {
+          // findOrCreate returns array
+          const [product] = body;
+          expect(product).to.be.an('object');
+          expect(product.name).to.be.equal(productToPost.name);
+          expect(product.image).to.be.equal(productToPost.image);
+          expect(product.description).to.be.equal(productToPost.description);
+          expect(product.price).to.be.equal(productToPost.price);
+          expect(product.quantity).to.be.equal(productToPost.quantity);
+          expect(product.categoryId).to.be.equal(productToPost.categoryId);
+        });
+    });
+
+    it('PUT /api/products', () => {
+      const update = {
+        description: 'Not an awesome card',
+        price: 0.99,
+        quantity: 999,
+      };
+
+      return request(app)
+        .put(`/api/products/${productId}`)
+        .send(update)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).to.be.an('object');
+          expect(body.name).to.be.equal(productObj.name);
+          expect(body.image).to.be.equal(productObj.image);
+          expect(body.description).to.be.equal(update.description);
+          expect(body.price).to.be.equal(update.price);
+          expect(body.quantity).to.be.equal(update.quantity);
+          expect(body.categoryId).to.be.equal(productObj.categoryId);
+        });
+    });
+
+    it('DELETE /api/products', () => {
+      return request(app)
+        .delete(`/api/products/${productId}`)
+        .expect(204)
+        .then(() => {
+          Product.findById(productId)
+            .then((product) => {
+              expect(product).to.be.equal(null);
+            });
+        });
+    });
+  }); // end describe('/api/users')
+}); // end describe('User routes')
