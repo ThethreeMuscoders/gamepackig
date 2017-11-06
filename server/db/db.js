8b3880a26e89607695584020f6b426c0ddee7c9b
jessebarron <jessebarron1113@gmail.com>
2017-11-06 10:03:57 -0600

    Deleted a trailing comma hopefully it will deploy

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/gamepackig', {
     logging: false,
-  },
+  }
 );
 
 module.exports = db;
