23fa55e236f203ca903c79dbadce232111b5db79
jessebarron <jessebarron1113@gmail.com>
2017-11-07 22:16:46 -0600

    forgot to change the db port again

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5433/gamepackig', {
+  process.env.DATABASE_URL || 'postgres://localhost:5432/gamepackig', {
     logging: false,
   }
 );
